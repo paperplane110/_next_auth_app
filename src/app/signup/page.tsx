b9c70e1bcc3834744a885a1b97523a3cf9c34018
paperplane110 <1374736640@qq.com>
2023-08-02 23:17:10 +0800

    1:09:09

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -11,13 +11,46 @@ export default function SignupPage() {
         username: ""
     })
 
-    const onSignup =async () => {
+    const onSignup = async () => {
     }
 
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Signup</h1>
+            <br />
+            <label htmlFor="username">username</label>
+            <input
+                className="p-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600"
+                id="username"
+                type="text"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                placeholder="username"
+            />
+            <label htmlFor="email">email</label>
+            <input
+                className="p-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600"
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="email"
+            />
+            <label htmlFor="password">password</label>
+            <input
+                className="p-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="password"
+            />
+            <button
+                className="p-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600"
+                onClick={onSignup}
+            >Sign up</button>
+            <Link href="/login">Login</Link>
         </div>
     )
 }
