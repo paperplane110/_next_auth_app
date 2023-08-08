b5f6cb93c436fd0469cef18e9486b46b62849afb
paperplane110 <1374736640@qq.com>
2023-08-08 22:40:42 +0800

    1:50:02

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,23 +1,44 @@
 "use client"
 import Link from "next/link"
-import React from "react"
+import React, { useEffect } from "react"
 import { useRouter } from "next/navigation"
-import { axios } from "axios"
+import axios from "axios"
+import { toast } from "react-hot-toast"
 
 export default function SignupPage() {
+    const router = useRouter()
     const [user, setUser] = React.useState({
         email: "",
         password: "",
         username: ""
     })
-
+    const [buttonDisabled, setButtonDisabled] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
     const onSignup = async () => {
+        try {
+            setLoading(true)
+            const resp = await axios.post("/api/users/signup", user)
+            console.log("signup success", resp.data)
+            router.push('/login')
+        } catch (error: any) {
+            console.log("signup error", error.message)
+            toast.error(error.message)
+        } finally {
+            setLoading(false)
+        }
     }
 
+    useEffect(() => {
+        if (user.username.length > 0 && user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false)
+        } else {
+            setButtonDisabled(true)
+        }
+    }, [user])
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Signup</h1>
+            <h1>{loading ? "Processing" : "Signup"}</h1>
             <br />
             <label htmlFor="username">username</label>
             <input
@@ -49,7 +70,8 @@ export default function SignupPage() {
             <button
                 className="p-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600"
                 onClick={onSignup}
-            >Sign up</button>
+                disabled={buttonDisabled}
+            >Signup</button>
             <Link href="/login">Login</Link>
         </div>
     )
